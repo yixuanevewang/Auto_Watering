34a91521ad1ba01cdcda6573ecf402d59ddc2643
Eve Wang <92340569+yixuanevewang@users.noreply.github.com>
2022-08-15 11:26:46 -0400

    Add files via upload

---
+++ auto_watering.ino
@@ -0,0 +1,27 @@
+//const int DRYNESS_THRESHOLD = 200;
+
+const int pump = 11;
+const int soil_moisture = 12;
+
+void setup() { 
+  // put your setup code here, to run once:
+  pinMode(pump, OUTPUT);
+  pinMode(soil_moisture, INPUT);
+  Serial.begin(9600);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int moistureReading = digitalRead(soil_moisture);
+  Serial.println(moistureReading);
+  if(moistureReading == 0) 
+  {
+    digitalWrite(pump, HIGH);
+  }
+  else {
+    digitalWrite(pump, LOW);
+  }
+ 
+  delay(2000);
+}
